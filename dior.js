a06785b2dae1be41cb9193c92558795cd7344dbd
itz07nishanth <sekarnishanth46@gmail.com>
2025-07-28 17:28:56 +0530

    code
    dgf

---
+++ dior.js
@@ -0,0 +1,36 @@
+function btnOpen(){
+    const over = document.querySelector(".toggle")
+    over.classList.add("show")
+    over.classList.remove("hide")
+    console.log(over)
+}
+function btnClose(){
+    const over = document.querySelector(".toggle")
+    over.classList.add("hide")
+    over.classList.remove("show")
+    console.log(over)
+}
+function buttonsOpen(){
+  const over = document.querySelector(".pop-show")
+  over.classList.add("show")
+  over.classList.remove("hide")
+  console.log(over)
+}
+function buttonClose(){
+    const over = document.querySelector(".pop-show")
+    over.classList.add("hide")
+    over.classList.remove("show")
+    console.log(over)
+}
+function btnsOpen(){
+    const over = document.querySelector(".demo-show")
+    over.classList.add("show")
+    over.classList.remove("hide")
+    console.log(over)
+}
+function btnsClose(){
+    const over =  document.querySelector(".demo-show")
+    over.classList.add("hide")
+    over.classList.remove("show")
+    console.log(over)
+}
